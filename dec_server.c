7268f998d208643300ca89885c38de999a1c20e3
David Green <djoeyg7@gmail.com>
2022-08-25 10:27:43 -0700

    Uploaded project files to public Github repository.

---
+++ dec_server.c
@@ -0,0 +1,257 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+
+// Global variable tracks number of child processes
+static volatile sig_atomic_t CHILD_SOCKETS = 0;
+
+// Error function to report issues and exit process
+void error(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(1); 
+}
+
+/* use signal handler to reap all dead processes
+*  Source citation: Beej's Guide To Network Programming, Section 6.1 A Simple Stream Server
+*  https://beej.us/guide/bgnet/html/#a-simple-stream-server
+*/
+void sigchld_handler(int signo)
+{
+    // Decrement number of active child processes for each process reaped
+    int saved_errno = errno;
+    while(waitpid(-1, NULL, WNOHANG) > 0) {
+        CHILD_SOCKETS--;
+    }
+    errno = saved_errno;
+}
+
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address, int portNumber) {
+ 
+    // Clear out the address struct
+    memset((char*) address, '\0', sizeof(*address)); 
+    // The address should be network capable
+    address->sin_family = AF_INET;
+    // Store the port number
+    address->sin_port = htons(portNumber);
+    // Allow a client at any address to connect to this server
+    address->sin_addr.s_addr = INADDR_ANY;
+}
+
+int main(int argc, char *argv[]) {
+    int connectionSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+    socklen_t sizeOfClientInfo = sizeof(clientAddress);
+
+    // Check usage & args
+    if (argc < 2) { 
+        fprintf(stderr,"USAGE: %s port\n", argv[0]); 
+        exit(1);
+    }
+
+    // Create the socket that will listen for connections
+    int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket < 0) {
+        error("ERROR opening socket");
+    }
+
+    // Set up the address struct for the server socket
+    setupAddressStruct(&serverAddress, atoi(argv[1]));
+
+    // Associate the socket to the port
+    if (bind(listenSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+        error("ERROR on binding");
+    }
+
+    // Start listening for connetions. Allow up to 5 connections to queue up
+    listen(listenSocket, 5);
+
+    /* use signal handler to reap all dead processes
+     * Source citation: Beej's Guide To Network Programming, Section 6.1 A Simple Stream Server
+     * https://beej.us/guide/bgnet/html/#a-simple-stream-server
+    */
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler; 
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        error("sigaction");
+    }
+
+    int msgCharsRead, keyCharsRead, ackCharsRead;
+    int ackCharsWritten, decCharsWritten;
+    size_t bufLen = 256;
+    char msgBuffer[bufLen];
+    char keyBuffer[bufLen];
+    char ackBuffer[bufLen];
+    char decBuffer[bufLen];
+  
+    // Accept a connection, blocking if one is not available until one connects
+    while(1) {
+    
+        // Accept only up to 5 socket connections at one time
+        if (CHILD_SOCKETS <= 5) {
+
+            // Accept the connection request which creates a connection socket
+            connectionSocket = accept(listenSocket, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); 
+            if (connectionSocket < 0){
+                error("ERROR on accept");
+            }
+
+            /* Handle each client request in a new child process
+            *  Source Citation: The Linux Programming Interface, Author: Michael Kerrisk
+            *  Chapter 60.3 - Listing 60.4, page 1,245
+            */
+            switch (fork()) {
+            case -1:
+                fprintf(stderr, "Can't create child process\n");
+                close(connectionSocket);
+                break;
+
+            // Child Process
+            case 0:
+                // increment count of active child processes                       
+                CHILD_SOCKETS++;
+
+                // Close unneeded copy of listening socket
+                close(listenSocket);        
+
+                memset(ackBuffer, '\0', sizeof(ackBuffer));
+
+                // Establish handshake with client socket
+                ackCharsRead = recv(connectionSocket, ackBuffer, sizeof(ackBuffer) - 1, 0); 
+                if (ackCharsRead < 0){
+                    error("ERROR reading from socket");
+                }
+
+                // Send servier identification acknowledgment back to the client
+                ackCharsWritten = send(connectionSocket, "dec_server", 10, 0); 
+                if (ackCharsWritten < 0){
+                    error("ERROR writing to socket");
+                }
+
+                // Refuse and close connection to wrong client
+                if (strcmp(ackBuffer, "dec_client") != 0 ) {
+                    close(connectionSocket);
+                    exit(2);
+                }
+
+                while(1) {
+
+                    memset(msgBuffer, '\0', sizeof(msgBuffer));
+
+                    // Read the client's message from the socket
+                    msgCharsRead = recv(connectionSocket, msgBuffer, sizeof(msgBuffer) - 1, 0); 
+                    if (msgCharsRead < 0){
+                        error("ERROR reading from socket");
+                    }
+
+                    // Stop processing data message sent from client
+                    if (strcmp(msgBuffer, "STOP") == 0 ) {
+                        break;
+                    } 
+
+                    // Send an acknowledgment message back to the client
+                    ackCharsWritten = send(connectionSocket, "Server acknowledgment", 21, 0); 
+                    if (ackCharsWritten < 0){
+                        error("ERROR writing to socket");
+                    }
+
+                    memset(keyBuffer, '\0', sizeof(keyBuffer));
+
+                    // Read the client's encryption key from the socket
+                    keyCharsRead = recv(connectionSocket, keyBuffer, sizeof(keyBuffer) - 1, 0); 
+                    if (keyCharsRead < 0){
+                        error("ERROR reading from socket");
+                    }
+
+                    if( keyCharsRead < msgCharsRead) {
+                        // key can be larger than message but not shorter
+                        error("Error: key is too short"); 
+                    }
+
+                    /********** Decrypt message & send back data to client **********/ 
+
+                    char charList[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+                    memset(decBuffer, '\0', sizeof(decBuffer));
+                    int errCh, msgCh, keyCh, decCh;
+
+                    for(int i = 0; i < strlen(msgBuffer); i++) {
+
+                        errCh = msgBuffer[i];
+                        // Check for "bad" characters in ciphertext file
+                        if(( errCh < 65 || errCh > 90) && errCh != 32) {
+                            error("dec_server error: ciphertext contains bad characters");
+                        }
+
+                        if( msgBuffer[i] == ' ') {
+                            msgCh = 26;
+                        } else {
+                            msgCh = (msgBuffer[i] - 65);
+                        }
+
+                        errCh = keyBuffer[i];
+                        // Check for "bad" characters in key file
+                        if(( errCh < 65 || errCh > 90) && errCh != 32) {
+                            error("dec_server error: key contains bad characters");
+                        }
+
+                        if( keyBuffer[i] == ' ') {
+                            keyCh = 26;
+                        } else {
+                            keyCh = (keyBuffer[i] - 65);
+                        }
+
+                        // put new decrypted character into buffer
+                        decCh = msgCh - keyCh;
+                        if( decCh < 0) {
+                            decCh += 27;
+                            decBuffer[i] = charList[decCh];
+                        } else {
+                            decBuffer[i] = charList[decCh % 27];
+                        }
+                    }
+
+                    // Add newline character to end of encoded string
+                    if( strlen(msgBuffer) < bufLen - 1) {
+                        decBuffer[strlen(msgBuffer)] = '\n';
+                    }
+
+                    // Send decrypted message back to the client
+                    do {
+                        decCharsWritten = send(connectionSocket, decBuffer, strlen(decBuffer), 0); 
+                        if (decCharsWritten < 0){
+                            error("ERROR Server writing to socket");
+                        }
+                        if (decCharsWritten < strlen(decBuffer)){
+                            fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+                        }
+                    } while (decCharsWritten < strlen(decBuffer));
+                }
+                // Close the connection socket for this client
+                close(connectionSocket); 
+                _exit(EXIT_SUCCESS);
+
+            // Parent Process
+            default:
+                // Close unneeded copy of connected socket
+                close(connectionSocket);
+
+                // Loop to accept next connection
+                break;                      
+            }
+        }
+    }
+    // Close the listening socket
+    close(listenSocket); 
+    return 0;
+}
---
+++ enc_client.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
+#include <fcntl.h>
+
+// Error function used for reporting issues
+void error(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(1); 
+}
+
+// Set up the address struct
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber, 
+                        char* hostname) {
+ 
+    // Clear out the address struct
+    memset((char*) address, '\0', sizeof(*address));
+
+    // The address should be network capable
+    address->sin_family = AF_INET;
+
+    // Store the port number
+    address->sin_port = htons(portNumber);
+
+    // Get the DNS entry for this host name
+    struct hostent* hostInfo = gethostbyname(hostname); 
+    if (hostInfo == NULL) { 
+        error("CLIENT: ERROR, no such host"); 
+    }
+
+    // Copy the first IP address from the DNS entry to sin_addr.s_addr
+    memcpy((char*) &address->sin_addr.s_addr, 
+        hostInfo->h_addr_list[0],
+        hostInfo->h_length);
+}
+
+int main(int argc, char *argv[]) {
+
+    /*
+    * Encryption Client
+    * args: 0 enc_client 1 plaintext 2 key 3 port
+    * connects to localhost
+    */
+
+    int socketFD, portNumber; 
+    int msgCharsWritten, keyCharsWritten;
+    int ackCharsRead, encCharsRead;
+    struct sockaddr_in serverAddress;
+    size_t bufLen = 256;
+    char msgBuffer[bufLen];
+    char keyBuffer[bufLen];
+    char ackBuffer[bufLen];
+    char encBuffer[bufLen];
+
+    // Check usage & args
+    if (argc < 4) { 
+        fprintf(stderr,"USAGE: %s plaintext key port\n", argv[0]); 
+        exit(1); 
+    }
+
+    // Open the plaintext file to get input message
+    FILE *plainTextFD = fopen(argv[1], "r");
+    if (plainTextFD == NULL) { 
+        fprintf(stderr, "cannot open %s for input\n", argv[1]); 
+        exit(1); 
+    }
+
+    // Open the key file to get encryption key
+    FILE *keyFD = fopen(argv[2], "r");
+    if (keyFD == NULL) { 
+        fprintf(stderr, "cannot open %s for input\n", argv[2]); 
+        exit(1); 
+    }
+
+    // Determine length of plaintext and key files
+    struct stat msgStat;
+    struct stat keyStat;
+    stat(argv[1], &msgStat);
+    ssize_t msgLen = msgStat.st_size;
+    stat(argv[2], &keyStat);
+    ssize_t keyLen = keyStat.st_size;
+
+    // Validate length of key compared to length of plaintext
+    if( keyLen < msgLen) {
+
+            // key can be larger than message but not shorter
+            fprintf(stderr, "Error: %s is too short\n", argv[2]); 
+            exit(1);
+        }
+
+    // Create a socket
+    socketFD = socket(AF_INET, SOCK_STREAM, 0); 
+    if (socketFD < 0){
+        error("CLIENT: ERROR opening socket");
+    }
+
+    // Set up the server address struct
+    setupAddressStruct(&serverAddress, atoi(argv[3]), "localhost");
+
+    // Connect to server
+    if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0){
+
+        // Report connetion error with the attempted port, and set exit value to 2.
+        fprintf(stderr, "Error: could not contact enc_server on port %s\n", argv[3]);
+        exit(2);
+    }
+
+    // Establish handshake with server socket 
+    msgCharsWritten = send(socketFD, "enc_client", 10, 0);
+    if (msgCharsWritten < 0) {
+        error("CLIENT: ERROR writing handshake ACK to socket");
+    }
+
+    // Clear out the acknowledgement msg buffer
+    memset(ackBuffer, '\0', sizeof(ackBuffer));
+
+    // Get acknowledgment message from server
+    ackCharsRead = recv(socketFD, ackBuffer, sizeof(ackBuffer) - 1, 0); 
+    if (ackCharsRead < 0){
+        error("CLIENT: ERROR reading ACK from socket");
+    }
+
+    // Report connetion error with the attempted port, and set exit value to 2. 
+    if (strcmp(ackBuffer, "enc_server") != 0 ) {
+        fprintf(stderr, "Error: could not connect to %s on port %s\n", ackBuffer, argv[3]);
+        close(socketFD);
+        exit(2);
+    }
+
+    /********** Read, Send, Receive, Write data **********/
+
+    // initialize buffers and count the bytes read into buffers
+    ssize_t m = 0;
+    ssize_t k = 0;
+
+    while(m < msgLen) {
+
+        memset(msgBuffer, '\0', sizeof(msgBuffer));
+        memset(keyBuffer, '\0', sizeof(keyBuffer));
+        m += fread(msgBuffer, sizeof(char), bufLen - 1, plainTextFD);
+        k += fread(keyBuffer, sizeof(char), bufLen - 1, keyFD);
+
+        // Validate length of key compared to length of plaintext
+        if( k < m) {
+            // key can be larger than message but not shorter
+            fprintf(stderr, "Error: %s is too short\n", argv[2]); 
+            exit(1);
+        }
+
+        // Remove the trailing \n at end of strings
+        msgBuffer[strcspn(msgBuffer, "\n")] = '\0';
+        keyBuffer[strcspn(keyBuffer, "\n")] = '\0';
+
+        // Check for "bad" characters in plaintext and key files
+        for(int i = 0; i < strlen(msgBuffer); i++) {
+
+            if(( msgBuffer[i] < 65 || msgBuffer[i] > 90) && msgBuffer[i] != 32) {
+                // Send end of data message to server if bad character found and exit
+                msgCharsWritten = send(socketFD, "STOP", 4, 0);
+                if (msgCharsWritten < 0) {
+                    error("CLIENT: ERROR writing 'end of data' to socket");
+                }
+                error("enc_client error: plaintext contains bad characters\n");
+            }
+            if(( keyBuffer[i] < 65 || keyBuffer[i] > 90) && keyBuffer[i] != 32) {
+                // Send end of data message to server if bad character found and exit
+                msgCharsWritten = send(socketFD, "STOP", 4, 0);
+                if (msgCharsWritten < 0) {
+                    error("CLIENT: ERROR writing 'end of data' to socket");
+                }
+                error("enc_client error: key contains bad characters\n");
+            }
+        }
+
+        // send message file contents from buffer to server socket
+        do { 
+            msgCharsWritten = send(socketFD, msgBuffer, strlen(msgBuffer), 0);
+
+            if (msgCharsWritten < 0) {
+                error("CLIENT: ERROR writing msg data to socket");
+            }
+            if (msgCharsWritten < strlen(msgBuffer)){
+                fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+            }
+        } while (msgCharsWritten < strlen(msgBuffer));
+
+        // Clear out the acknowledgement msg buffer
+        memset(ackBuffer, '\0', sizeof(ackBuffer));
+
+        // Get acknowledgment message from server
+        ackCharsRead = recv(socketFD, ackBuffer, sizeof(ackBuffer) - 1, 0); 
+        if (ackCharsRead < 0) {
+            error("CLIENT: ERROR reading ACK from socket");
+        }
+
+        // send key file contents from buffer to server socket
+        do {
+            keyCharsWritten = send(socketFD, keyBuffer, strlen(keyBuffer), 0);
+            if (keyCharsWritten < 0){
+                error("CLIENT: ERROR writing key data to socket");
+            }
+            if (keyCharsWritten < strlen(keyBuffer)){
+                fprintf(stderr, "CLIENT: WARNING: Not all data written to socket!\n");
+            }
+        } while (keyCharsWritten < strlen(keyBuffer));
+
+        // Clear out the encrypted msg buffer
+        memset(encBuffer, '\0', sizeof(encBuffer));
+
+        // Get encrypted message back from server
+        encCharsRead = recv(socketFD, encBuffer, sizeof(encBuffer) - 1, 0); 
+        if (encCharsRead < 0){
+            error("CLIENT: ERROR reading from socket");
+        }
+        fprintf(stdout, "%s", encBuffer);
+        fflush(stdout);
+    }
+
+    // Send end of data message to server
+    msgCharsWritten = send(socketFD, "STOP", 4, 0);
+    if (msgCharsWritten < 0) {
+        error("CLIENT: ERROR writing 'end of data' to socket");
+    }
+
+    // Close the socket, plaintext and key files and return
+    fclose(plainTextFD);
+    fclose(keyFD);
+    close(socketFD); 
+    return 0;
+}
